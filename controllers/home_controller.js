309e112a17f49fa7cc75f400102bbb6071288e41
jagadeesh <vyomanuatjagadeesh@gmail.com>
2023-08-05 16:52:04 +0530

    it is my final commit to the server

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,10 @@
-module.exports.home = (req, res)=>{
-   return res.end("controller is working");
+
+module.exports.home = function(req, res){
+  return res.render('home',{
+   title:"Home"
+  });
+  
 }
+
+
+// Module.export.actionName = funection(req, res)
--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const app = express();
 const port = 8000;
 
 // using the routers
-app.use('./', require('./routes'));
-app.set('view engine', ejs);
-app,set('views', './views');
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
+// app.get('/', (req,res)=>{
+//     res.end("it is working")
+// })
+app.use('/', require('./routes'));
 app.listen(port, (err)=>{
     if(err){
         console.log(`Error in running the server: ${err}`);
     }
+
     else{
         console.log(`The server is running on ${port}`);
     }
